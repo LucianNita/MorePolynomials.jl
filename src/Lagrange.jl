48b0897d2372616bb6206c44e9222db470519723
sprockmonty <sprockmonty@gmail.com>
2020-04-22 03:49:50 +0100

    Large performance improvements to lagrange interpolation and weight generation

--- src/Lagrange.jl
+++ src/Lagrange.jl
@@ -23,31 +23,45 @@ end
 mutable struct LagrangePoly{T<:Number} <: AbstractLagrangePolynomial{T}
     x::Vector{T}
     y::Vector{T}
-    weights::Vector
+    weights::Vector{T}
     domain::Interval
     var::Symbol
     function LagrangePoly{T}(x::Vector{T}, y::Vector{T}, domain::Interval, var::Symbol) where {T <: Number}
-        return new{T}(x,y,lagrange_bary_weights(x),domain, var)
+        return new{T}(x,y,lagrange_bary_weights(x,T),domain, var)
     end
 end
 @register LagrangePoly
 LagrangePoly(x::AbstractVector{T},y::AbstractVector{T}, domain::Interval, var::SymbolLike = :x) where {T} = LagrangePoly{T}(x,y,domain,Symbol(var))
 
-function lagrange_bary_weights(x)
+function lagrange_bary_weights(x::AbstractVector{T}, ::Type{T}) where {T}
     numPoints = length(x)
-    weights = [prod(1/(x[i] - x[j]) for j in 1:numPoints if i ≠ j) for i in 1:numPoints]
-    return weights
+    w = ones(T,numPoints)
+    for j in 2:numPoints
+        @inbounds xj = x[j]
+        for k in 1:j-1
+            @inbounds w[k] *= x[k] - xj
+        end
+        for k in 1:j-1
+            @inbounds w[j] *= xj - x[k]
+        end
+    end
+    return 1 ./ w
 end
 
-function lagrange_eval_weights(p::AbstractLagrangePolynomial, xeval, y)
+function lagrange_eval_weights(p::AbstractLagrangePolynomial{T}, xeval, y) where {T}
     x = p.x
-    any(x.==xeval) && return y[x.==xeval][1]
     w = p.weights
-    num = mapreduce((x,y,w)-> w*y / (xeval - x),+,x,y,w)
-    denom = mapreduce((x,w)-> w / (xeval - x),+,x,w)
-    val = num/denom
-    return val
+    num = zero(T)
+    denom = zero(T)
+    for j = 1:length(x)
+        @inbounds wx = w[j] / (xeval - x[j])
+        isinf(wx) && return y[j]
+        @inbounds num += wx*y[j]
+        denom +=wx
+    end
+    return num/denom
 end
+
 
 domain(p::AbstractLagrangePolynomial) = p.domain
 LagrangePoly(x::Vector{T}, y::Vector{T}, var::Symbol=:x; lower = min(x...), upper = max(x...)) where {T} = LagrangePoly(x,y,Interval(lower, upper),var)
@@ -60,29 +74,28 @@ update!(p::AbstractLagrangePolynomial{T}, y::Vector{T}) where {T} = p.y = y
 # Legendre-Gauss-Radau
 
 mutable struct LGRPoly{T<:Number} <: AbstractLagrangePolynomial{T}
-    x::Vector
+    x::Vector{T}
     y::Vector{T}
-    weights::Vector
+    weights::Vector{T}
     var::Symbol
     function LGRPoly{T}(y::Vector{T}, var::Symbol) where {T <: Number}
         lgrpoints = lgr_points(length(y))
-        return new{T}(lgrpoints,y,lagrange_bary_weights(lgrpoints), var)
+        return new{T}(lgrpoints,y,lagrange_bary_weights(lgrpoints, T), var)
     end
 end
 @register LGRPoly
 LGRPoly(y::AbstractVector{T}, var::SymbolLike = :x) where {T} = LGRPoly{T}(y, Symbol(var))
 
 function lgr_points(order) # does not include endpoints
-    if order>1
-        N = 1:order-2
-        a = zeros(order-1)
-        b = zeros(order-2)
-        a[1] = 1 / 3 
-        a[2:end] = map((N)-> 1 / (4*N^2 + 8N +3),N)
-        b = map((N)-> (N^2 + N)^0.5 / (2N+1),N)
-        J = SymTridiagonal(a,b)
-        return pushfirst!(eigvals(J),-1)
-    end
+    order<2 && error("order must be greater than one")
+    N = 1:order-2
+    a = zeros(order-1)
+    b = zeros(order-2)
+    a[1] = 1 / 3 
+    a[2:end] = map((N)-> 1 / (4*N^2 + 8N +3),N)
+    b = map((N)-> (N^2 + N)^0.5 / (2N+1),N)
+    J = SymTridiagonal(a,b)
+    return pushfirst!(eigvals(J),-1)
 end
 
 domain(P::Type{<:LGRPoly}) = Interval(-1,1,true, false)
