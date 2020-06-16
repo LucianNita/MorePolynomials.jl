968921d84d2261ef2e68e72a6b9ed7ac293a1f87
sprockmonty <sprockmonty@gmail.com>
2020-06-16 01:55:52 +0100

    added new functions to export

--- src/IntervalsExtras.jl
+++ src/IntervalsExtras.jl
@@ -1,13 +1,13 @@
 
 # some Intervals functions that make my life easier
 
-containslower(i::Interval) = first(inclusivity(i))
-containsupper(i::Interval) = last(inclusivity(i))
+containsfirst(i::Interval) = first(inclusivity(i))
+containslast(i::Interval) = last(inclusivity(i))
 
-function convert(::Type{Interval{T}}, i::Interval{N}) where {T,N}
+function Base.convert(::Type{Interval{T}}, i::Interval{N}) where {T,N}
     ifirst = convert(T, first(i))
     ilast = convert(T, last(i))
-    return Interval(ifirst, ilast, containslower(i), containsupper(i) )
+    return Interval(ifirst, ilast, containsfirst(i), containslast(i) )
 end
 
 Base.promote_rule(::Type{Interval{T}}, ::Type{Interval{S}}) where {T,S} = Interval{promote_type(T,S)}
